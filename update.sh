087428a61e6bf417a11feba0b1517c7bd12c9f67
0xDK <31103434+0xdkcvd@users.noreply.github.com>
2025-02-03 17:35:33 +0700

    Create update.sh

---
+++ update.sh
@@ -0,0 +1,11 @@
+# Download latest release
+latest_tag=$(curl -s https://api.github.com/repos/t3rn/executor-release/releases/latest | grep -Po '"tag_name": "\K.*?(?=")')
+filename="executor-linux-${latest_tag}.tar.gz"
+
+wget "https://github.com/t3rn/executor-release/releases/download/${latest_tag}/${filename}"
+
+# Extract the downloaded tar.gz file
+tar -xzf "$filename"
+rm "$filename"
+
+echo "Update success!"
